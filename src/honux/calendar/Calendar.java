9b13b2931a43105c51b560f7630a0969ea6dd942
YES2TOP <47927297+YES2TOP@users.noreply.github.com>
2020-11-14 10:59:02 -0600

    up

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -9,7 +9,15 @@ public class Calendar {
 		return MAX_DAYS[month-1];
 	}
 	
-	
+	public void printSampleCalendar() {
+		System.out.println(" 일   월   화   수   목   금   토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		
+	}
 	
 	
 	public static void main(String[] args) {
@@ -31,6 +39,7 @@ public class Calendar {
 //		int[] maxDays= {31,28,31,30,31,30,31, 31, 30, 31, 30,31};
 		
 		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		cal.printSampleCalendar();
 		scanner.close();
 	}
 }
